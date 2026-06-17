fe3fc769a5a35f7d4254e68754420b4c91507db6
Sanal da Silva <tomazsanal@gmail.com>
2026-06-17 00:18:30 -0300

    feat: criação da entidade de Usuario

---
+++ src/main/java/com/alura/vollmed/domain/usuario/Usuario.java
@@ -0,0 +1,22 @@
+package com.alura.vollmed.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+    private String senha;
+}
+
