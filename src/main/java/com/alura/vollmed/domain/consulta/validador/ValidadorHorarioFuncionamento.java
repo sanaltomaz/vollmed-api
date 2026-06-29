72496c1d8fa714f45ec33bcd3f7d20552df0c281
Sanal da Silva <tomazsanal@gmail.com>
2026-06-29 11:13:30 -0300

    fix(consulta): corrigir validação de horário de funcionamento

--- src/main/java/com/alura/vollmed/domain/consulta/validador/ValidadorHorarioFuncionamento.java
+++ src/main/java/com/alura/vollmed/domain/consulta/validador/ValidadorHorarioFuncionamento.java
@@ -5,6 +5,7 @@ import com.alura.vollmed.domain.consulta.ValidacaoConsultaException;
 import org.springframework.stereotype.Component;
 
 import java.time.DayOfWeek;
+import java.time.LocalTime;
 
 @Component
 public class ValidadorHorarioFuncionamento implements ValidadorAgendamento{
@@ -14,6 +15,12 @@ public class ValidadorHorarioFuncionamento implements ValidadorAgendamento{
         var domingo = dados.data().getDayOfWeek().equals(DayOfWeek.SUNDAY);
         var horarioDeAgendamento = dados.data().getHour();
 
+        var inicioConsulta = dados.data();
+        var fimConsulta = inicioConsulta.plusHours(1);
+
+        var fechamento = LocalTime.of(19,0)
+                .atDate(inicioConsulta.toLocalDate());
+
         if (domingo) {
             throw new ValidacaoConsultaException("A clínica não funciona aos domingos.");
         }
@@ -22,8 +29,10 @@ public class ValidadorHorarioFuncionamento implements ValidadorAgendamento{
             throw new ValidacaoConsultaException("Agendamento de horários somente após as 07:00");
         }
 
-        if (horarioDeAgendamento > 18) {
-            throw new ValidacaoConsultaException("Agendamento de horários somente até as 18:00");
+        if (fimConsulta.isAfter(fechamento)) {
+            throw new ValidacaoConsultaException(
+                    "O último horário disponível para início da consulta é às 18:00."
+            );
         }
     }
 }
